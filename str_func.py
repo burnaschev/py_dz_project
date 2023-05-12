de86473c1e5bc6d0c425f87445e823f3353766a8
burnaschev <informal201144@gmail.com>
2023-05-12 09:44:57 +0400

    added func str_title and added docstring

--- str_func.py
+++ str_func.py
@@ -1,2 +1,9 @@
 def str_upper(value):
+    """Вывод слов большими буквами"""
     return value.upper()
+
+
+def str_title(value):
+    """Вывод каждого слова с
+    большой буквы"""
+    return value.title()
